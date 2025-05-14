543bf00e5344ee507e7867b2a1bda2c5c209b5ea
Klement Tamás <klemitomi84@gmail.com>
2025-05-15 01:05:04 +0200

    Init knowledge-check.component
    knowledge-check.component.ts
    knowledge-check.component.html
    knowledge-check.component.scss
    
    Létrehozása

---
+++ webhosting-quiz/src/knowledge-check/knowledge-check.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge-check',
+  imports: [],
+  templateUrl: './knowledge-check.component.html',
+  styleUrl: './knowledge-check.component.scss'
+})
+export class KnowledgeCheckComponent {
+
+}
