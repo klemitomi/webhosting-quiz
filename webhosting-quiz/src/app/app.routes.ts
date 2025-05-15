8adefff76418437384996896746092e058c405a7
Klement Tamás <klemitomi84@gmail.com>
2025-05-15 02:34:19 +0200

    Routing/App.components
    Optimalizálás

--- webhosting-quiz/src/app/app.routes.ts
+++ webhosting-quiz/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { KnowledgeCheckComponent } from '../knowledge-check/knowledge-check.component';
+
+export const routes: Routes = [
+    { path: '', component: KnowledgeCheckComponent }
+];
--- webhosting-quiz/src/app/knowledge-check/quiz.service.spec.ts
+++ webhosting-quiz/src/app/knowledge-check/quiz.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Question } from './question.model';
 import { QuizService } from './quiz.service';
 
 describe('QuizService', () => {
   let service: QuizService;
+  const testQuestions: Question[] = [
+    {
+    question: 'Test question', 
+    options: ['A', 'B', 'C'], 
+    correctAnswer: 0
+    },
+    {
+      question: 'Test question 2',
+      options: ['D', 'E', 'F'],
+      correctAnswer: 1
+    }
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
